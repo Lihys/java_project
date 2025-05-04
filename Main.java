f4be7d1f504789d1f412e37d9baf7837f7469420
Lihys <166310066+Lihys@users.noreply.github.com>
2025-05-04 19:28:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,16 @@
 /*
 By:
-- Amy  319128989
-- Lihy  322532193
+- Amy Ovadia 319128989
+- Lihy Levy 322532193
 
  */
 
 package amy_lihy_project2;
-import java.util.Scanner;
 
 public class Main {
-
-
+    
     public static void main(String[] args) {
          new CollegeManager().run();
     }
-
-
-
-
-
+    
 }
